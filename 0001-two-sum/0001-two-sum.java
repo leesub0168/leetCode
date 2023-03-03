d45726d9f70a7e6d8e0b9355aac5ca1986fc7b48
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-03 15:18:56 +0900

    Time: 4 ms (66.51%), Space: 44.2 MB (17.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
-        Hashtable<Integer, Integer> table = new Hashtable<>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++) {
-            table.put(nums[i],i);
+            map.put(nums[i],i);
         }
         for(int i=0;i<nums.length;i++) {
-            Integer idx = table.get(target - nums[i]);
+            Integer idx = map.get(target - nums[i]);
             if(idx != null && idx != i) {
                 return new int[] {idx, i};
             }
