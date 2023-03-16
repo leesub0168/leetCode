7097056a77d6efe18fd92c1273bded1e5b7baed9
leesub0168 <53374789+leesub0168@users.noreply.github.com>
2023-03-17 01:03:44 +0900

    Time: 0 ms (100.00%), Space: 42.5 MB (30.75%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,33 +16,12 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null) return (root.val == targetSum);
-        List<Integer> list = new ArrayList<>();
-        if(root.left != null) {
-            bfs(root.left, root.val, list);
+        if(root.left == null && root.right == null && root.val == targetSum) return true;
+        if(hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)) {
+            return true;
+        }else {
+            return false;
         }
-        if(root.right != null) {
-            bfs(root.right, root.val, list);
-        }
-        
-        return list.contains(targetSum);
     }
     
-    public void bfs(TreeNode node, int sum, List<Integer> list) {
-        sum += node.val;
-        if(node.left == null && node.right == null) {
-            list.add(sum);
-            return;
-        }
-        if(node.left == null && node.right != null) {
-            bfs(node.right, sum, list);
-            return;
-        }
-        if(node.left != null && node.right == null) {
-            bfs(node.left, sum, list);
-            return;
-        }
-        bfs(node.left, sum, list);
-        bfs(node.right, sum, list);
-    }
 }
